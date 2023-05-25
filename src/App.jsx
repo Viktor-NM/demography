6357b80fd1e43c98d4b0330893d54b6b505a43f8
victor <victoby47@gmail.com>
2023-05-24 18:12:04 -0600

    Add team info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react'
 import Banner from './components/Banner'
 import Header from './components/Header'
 
 export default function App() {
+	const [openModal, setOpenModal] = useState(false)
 	return (
 		<div className='overflow:hidden flex flex-direction:col bg:gray-16 h:100vh '>
 			<Header />
 			<Banner />
+			<div>
+				<button onClick={()=>setOpenModal(true)} className='p:4|12 m:8 b:none r:4'>Información</button>
+			</div>
+			{
+				openModal && (<div tabIndex={-1} onClick={()=>setOpenModal(false)} className='z:4 bg:#6b6a6d55 abs w:100vw h:100vh d:flex ai:center jc:center '>
+				<section className='bg:white p:2rem r:10 d:grid gap:2rem jc:center'>
+					<header className='d:flex jc:space-between ai:center'>
+						<h2>Demography - ITA</h2>
+						<img className='w:82' src="http://it-acapulco.edu.mx/wp-content/themes/TecNMCampus/assets/img/logo-ita.png" alt="ita-logo" />
+					</header>
+					<nav className='d:grid grid-template-cols:1fr|1fr gap-x:2em'>
+						<p className='grid-col-span:2 t:24 t:bold mb:12'>Equipo 5</p>
+						<p className='t:left'>Nuñez Mendoza Victor Jesus</p>
+						<p>18320943</p>
+						<p className='t:left'>Mondragon Solano Cristopher</p>
+						<p>19520595</p>
+						<p className='t:left'>Vergara Barrientos Jesus Evedel</p>
+						<p>17321015</p>
+						<p className='t:left'>Yamamoto Lopez Manuel Goichi</p>
+						<p>17321017</p>
+					</nav>
+				</section>
+			</div>)
+			}
+			
 		</div>
 	)
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useData } from '../context/DataContext'
 export default function Header() {
 	const { setOption, topics, option } = useData()
 	return (
-		<header className='hidden block@md h:64@md flex@md flex-direction:col@md f:gray-60'>
+		<header className='hidden block@md h:64@md flex@md flex-direction:col@md f:gray-60 '>
 			<ul className='h:full flex gap:12 jc:center ai:center flex:wrap {list-style:none;p:4|6;cursor:pointer;~color|150ms}>li {f:gray-94}>li:hover'>
 				{topics.map((topic) => (
 					<li
@@ -14,7 +14,6 @@ export default function Header() {
 					</li>
 				))}
 			</ul>
-			<span className='w:75% bb:1|solid|gray-32 as:center'></span>
 		</header>
 	)
 }
